bddf5dbf0697766ae4f2d72eafbe152319a0681d
misawa.k <b1018211.fun@gmail.com>
2022-10-02 11:06:55 +0900

    modified:   src/App.css 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { css } from '@emotion/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={frame1}>
+      <p>hello misawa</p>
     </div>
   );
 }
 
 export default App;
+
+
+const frame1 = css`
+  height:40px;
+  width:40px;
+`
